98021fbf3797d2b8f9d15b41740ac05fae158dc4
T-Phong <T-Phong>
2025-02-26 22:16:33 +0700

    init

---
+++ push/Controllers/FirebaseService.cs
@@ -0,0 +1,33 @@
+﻿using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+public class FirebaseService
+{
+    public FirebaseService()
+    {
+        FirebaseApp.Create(new AppOptions()
+        {
+            Credential = GoogleCredential.FromFile("firebase-adminsdk.json")
+        });
+    }
+
+    public async Task<string> SendNotification(string token, string title, string body)
+    {
+        var message = new Message()
+        {
+            Token = token,
+            Notification = new Notification()
+            {
+                Title = title,
+                Body = body
+            }
+        };
+
+        string response = await FirebaseMessaging.DefaultInstance.SendAsync(message);
+        return response;
+    }
+}
---
+++ push/Controllers/PushController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+[ApiController]
+[Route("api")]
+public class PushController : ControllerBase
+{
+    private static List<string> tokens = new List<string>();
+    private readonly FirebaseService _firebaseService;
+
+    public PushController()
+    {
+        _firebaseService = new FirebaseService();
+    }
+
+    [HttpPost("save-token")]
+    public IActionResult SaveToken([FromBody] TokenRequest request)
+    {
+        if (!tokens.Contains(request.Token))
+        {
+            tokens.Add(request.Token);
+        }
+        return Ok(new { message = "Token đã lưu" });
+    }
+
+    [HttpPost("send-notification")]
+    public async Task<IActionResult> SendNotification([FromBody] NotificationRequest request)
+    {
+        foreach (var token in tokens)
+        {
+            await _firebaseService.SendNotification(token, request.Title, request.Body);
+        }
+        return Ok(new { message = "Thông báo đã gửi" });
+    }
+}
+
+public class TokenRequest
+{
+    public string Token { get; set; }
+}
+
+public class NotificationRequest
+{
+    public string Title { get; set; }
+    public string Body { get; set; }
+}
